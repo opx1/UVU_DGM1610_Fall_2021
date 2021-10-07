b931222462ccedbb284c28b3c6332add0159bf63
Natalie Rust <10840498@my.uvu.edu>
2021-10-06 21:53:49 -0600

    Added Variables and functions scripts

---
+++ Fall2021Class/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 5;
+
+    private void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
