90860b8644eee6f06407cf27e5589ac3f33aedb0
Natalie Rust <10840498@my.uvu.edu>
2021-10-07 21:02:04 -0600

    Added Name Code

--- Fall2021Class/Assets/MainPlayer.cs
+++ Fall2021Class/Assets/MainPlayer.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MainPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string myName;
     void Start()
     {
-      Debug.Log("I am alive!");  
+      Debug.Log("I am alive and my name is " + myName);  
     }
 
     // Update is called once per frame
