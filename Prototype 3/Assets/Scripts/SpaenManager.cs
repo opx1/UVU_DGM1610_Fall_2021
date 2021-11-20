d3c611d64cfdfdfe364d52309ab0b81cc6800690
Natalie Rust <10840498@my.uvu.edu>
2021-11-20 13:19:18 -0700

    Completed 3.1

---
+++ Prototype 3/Assets/Scripts/SpaenManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaenManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      InvokeRepeating("SpawnObstacle", startDelay, repeatRate);  
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
+    }
+}
